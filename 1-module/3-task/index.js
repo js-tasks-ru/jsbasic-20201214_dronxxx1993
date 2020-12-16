146942c6073b094d3e5a3cc552c6b55501f5b611
Андрей <dronxxx1993@yandex.ru>
2020-12-16 23:32:30 +0400

    feat:solved the third task from the first module

--- 1-module/3-task/index.js
+++ 1-module/3-task/index.js
@@ -4,5 +4,11 @@
  * @returns {string}
  */
 function ucFirst(str) {
-  // ваш код...
+  if (str.length === 1) {
+    return str.toUpperCase();
+  }
+  if (str === ''){
+    return ''
+  }
+  return `${str[0].toUpperCase()}${str.slice(1)}`
 }
