1f19bbaad9a34fca26508cb8870060110d43c301
Андрей <dronxxx1993@yandex.ru>
2021-01-04 17:00:12 +0500

    fixed: 4-module 3-task

--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -2,33 +2,42 @@
  * Метод устанавливает необходимые по условию аттрибуты таблице
  * @param {Element} table
  */
-function highlight(table) {
 
+const indexTableELaments = (table) => {
 
-  let rows = table.tHead.rows[0].cells;
-  let statusIndexTable;
-  let ageIndexTable;
-  let genderIndexTable;
+  const rows = table.tHead.rows[0].cells;
+  const indexTable = {};
+
   for (let i = 0; i < rows.length; i++) {
 
     if (rows[i].innerHTML === 'Status') {
-      statusIndexTable = rows[i].cellIndex;
+      indexTable.statusIndexTable = rows[i].cellIndex;
     } else if (rows[i].innerHTML === 'Age') {
-      ageIndexTable = rows[i].cellIndex;
+      indexTable.ageIndexTable = rows[i].cellIndex;
     } else if (rows[i].innerHTML === 'Gender') {
-      genderIndexTable = rows[i].cellIndex;
+      indexTable.genderIndexTable = rows[i].cellIndex;
     }
+
   }
-  let cell = table.rows;
+  return indexTable;
+};
+
+function highlight(table) {
+
+  const {ageIndexTable, genderIndexTable, statusIndexTable} = indexTableELaments(table);
+
+  const cell = table.rows;
 
   for (let j = 0; j < cell.length; j++) {
-    let age = cell[j].cells[ageIndexTable].innerHTML;
-    let gender = cell[j].cells[genderIndexTable].innerHTML;
-    let status = cell[j].cells[statusIndexTable];
+    const age = cell[j].cells[ageIndexTable].innerHTML;
+    const gender = cell[j].cells[genderIndexTable].innerHTML;
+    const status = cell[j].cells[statusIndexTable];
 
     if (age < 18) {
       cell[j].style = "text-decoration: line-through";
-    } else if (status.getAttribute('data-available') === 'true') {
+    }
+
+    if (status.getAttribute('data-available') === 'true') {
       cell[j].classList.add('available');
     } else if (status.getAttribute('data-available') === 'false') {
       cell[j].classList.add('unavailable');
@@ -40,10 +49,6 @@ function highlight(table) {
       cell[j].classList.add('female');
     } else if (gender === 'm') {
       cell[j].classList.add('male');
-    }
-
-    if (age < 18) {
-      cell[j].style = "text-decoration: line-through";
     }
 
   }
